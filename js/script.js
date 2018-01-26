777b0d68ae04d8c958079d57fdbedadeb49fab4d
madmerlln <35480514+madmerlln@users.noreply.github.com>
2018-01-26 11:47:49 +0000

    Correção lista de ingredientes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function viewProduct(productID) {
   product = potionD.potions[productID];
-
+  document.getElementById('ingredients').innerHTML = "";
   document.getElementById('imgProduct').src ="img/"+product.image;
   document.getElementById('name').innerHTML = product.name;
   document.getElementById('description').innerHTML = product.effect;
