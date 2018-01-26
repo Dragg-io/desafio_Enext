2b559c82783b77854856045f2754b3abd84ce52f
madmerlln <35480514+madmerlln@users.noreply.github.com>
2018-01-26 01:03:31 -0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function viewProduct(productID) {
+  product = potionD.potions[productID];
+
+  document.getElementById('imgProduct').src ="img/"+product.image;
+  document.getElementById('name').innerHTML = product.name;
+  document.getElementById('description').innerHTML = product.effect;
+  document.getElementById('price').innerHTML = "$"+product.price;
+  document.getElementById('productModal').style.display='block';
+	for(I in product.ingredients) {
+  	document.getElementById('ingredients').innerHTML += "<li>"+product.ingredients[I]+"</li>";
+  	I++;
+  }  
+
+}
